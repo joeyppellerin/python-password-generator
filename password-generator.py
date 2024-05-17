b9c64a3ee9b74df32f5684c12fc142805a690709
Joey Pelletier Pellerin <joeyp.pellerin@hotmail.com>
2024-05-16 22:12:15 -0400

    password generator complete

--- password-generator.py
+++ password-generator.py
@@ -1,51 +1,68 @@
 import string
 import random
 
-def convertStringToInteger(user_input, field):
+def convertStringToInteger(user_input, field_name):
   if user_input.isdigit():
     return int(user_input)
   else:
-    print(f'Try to enter a number the next time in field {field}.')
+    print(f'Try to enter a number the next time in field {field_name}.')
     return 0
+  
+def validateError(nb_characters, nb_digits, nb_special_caracters):
+  nb_total_other_characters = nb_digits + nb_special_caracters
+  if nb_total_other_characters > nb_characters:
+    print('The sum of the number of digits and the numbers of special characters entered are higher than the number of total characters. You will only get letters.\r\n')
+    return True
+  
+  return False
+  
+def getUserInput(question, field_name):
+  nb_characters_input = input(f'{question}\r\n')
+  return convertStringToInteger(nb_characters_input, field_name)
 
-nb_characters = 0
-nb_special_characters = 0
-nb_digits = 0
+def getListOfIndex(nb_other_characters, index_list):
+  list_of_index = []
+  for i in range(nb_other_characters):
+    digit = random.choice(index_list)
+    list_of_index.append(digit)
+    index_list.remove(digit)
 
-print('Générer son mot de passe complexe\r\n')
-print('---------------------------------\r\n')
-nb_characters_input = input('How many characters do you need in your your password?\r\n')
-nb_characters = convertStringToInteger(nb_characters_input, 'number of characters in password')
+  return list_of_index
 
-nb_digits_input = input('How many digits?\r\n')
-nb_digits = convertStringToInteger(nb_digits_input, 'number of digits')
+def generatePassword(nb_characters, digit_index_list, special_characters_index_list):
+  password = ''
+  for i in range(nb_characters):
+    if i in digit_index_list:
+      password += random.choice(string.digits)
+    elif i in special_characters_index_list:
+      password += random.choice(string.punctuation)
+    else:
+      password += random.choice(string.ascii_letters)
 
-nb_special_caracters_input = input('How many special characters?\r\n')
-nb_special_caracters = convertStringToInteger(nb_special_caracters_input, 'number of special characters')
+  return password
 
-digit_index_list = []
-for i in range(nb_digits):
-  digit_index_list.append(random.sample(list(range(0, nb_characters))))
+def main():
+  print('Générer son mot de passe complexe\r\n')
+  print('---------------------------------\r\n')
+  nb_characters = getUserInput('How many characters do you need in your your password?', 'number of characters in password')
+  nb_digits = getUserInput('How many digits?', 'number of digits')
+  nb_special_caracters = getUserInput('How many special characters?', 'number of special characters')
+  index_list = list(range(0, nb_characters))
+  
+  if validateError(nb_characters, nb_digits, nb_special_caracters):
+    nb_digits = 0
+    nb_special_caracters = 0
 
-special_characters_index_list = []
-for i in range(nb_special_caracters):
-  print(i)
-  special_characters_index_list.append(random.sample(list(range(0, nb_characters))))
+  digit_index_list = []
+  if nb_digits > 0:
+    digit_index_list = getListOfIndex(nb_digits, index_list)
 
-print(digit_index_list)
-print(special_characters_index_list)
+  special_characters_index_list = []
+  if nb_special_caracters > 0:
+    special_characters_index_list = getListOfIndex(nb_special_caracters, index_list)
 
-password = ''
-for i in range(nb_characters):
-  if i in digit_index_list:
-    password += random.choice(string.digits)
-  elif i in special_characters_index_list:
-    password += random.choice(string.punctuation)
-  else:
-    password += random.choice(string.ascii_letters)
+  print('\r\n')
+  print('---------------------------------\r\n')
+  print(f'Your password: {generatePassword(nb_characters, digit_index_list, special_characters_index_list)}')
 
-
-print('\r\n')
-print('---------------------------------\r\n')
-print(f'Your password: {password}')
-
+main()
